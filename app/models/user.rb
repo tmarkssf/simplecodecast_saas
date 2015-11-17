175b3e74178356d85e41268fe127225afcda69e3
Takezo Marks <tmarkssf@gmail.com>
2015-11-17 04:46:23 +0000

    most updated files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   belongs_to :plan
+  attr_accessor :stipe_card_token
   
   def save_with_payment
     if valid?
