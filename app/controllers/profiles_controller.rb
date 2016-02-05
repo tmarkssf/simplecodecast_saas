4fb94073ba3563f105136c43bc96a39e1615e5bc
Takezo Marks <tmarkssf@gmail.com>
2016-02-05 07:31:20 +0000

    Added profiles new action

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+    def new
+        #form for user to fill out their own profile
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile
+    end
+end
--- db/migrate/20151105062033_create_plans.rb
+++ db/migrate/20151105062033_create_plans.rb
@@ -1,6 +1,7 @@
 class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
+      t.integer :plan_id
       t.string :name
       t.decimal :price
     
